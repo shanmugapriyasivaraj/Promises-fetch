0bdf73f12f9ed128df98e774f9de8843821d415d
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-03-19 13:24:42 +0530

    Promise chaining

--- script.js
+++ script.js
@@ -20,4 +20,29 @@ let order = (time, work) => {
   });
 };
 
-order(2000, () => console.log(`${stocks.Fruits[0]} was selected`));
+order(2000, () => console.log(`${stocks.Fruits[0]} was selected`))
+  .then(() => {
+    return order(0000, () => console.log("Production has started"));
+  })
+  .then(() => {
+    return order(2000, () => console.log("the fruit was chopped"));
+  })
+  .then(() => {
+    console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]}`);
+  })
+  .then(() => {
+    return order(1000, () => console.log("Start the machine"));
+  })
+  .then(() => {
+    return order(2000, () => {
+      console.log(`Icecream Placed on ${stocks.holder[0]}`);
+    }).then(() => {
+      return order(3000, () => {
+        console.log(`${stocks.toppings[0]} was selected`);
+      }).then(() => {
+        return order(1000, () => {
+          console.log("Icecream was served");
+        });
+      });
+    });
+  });
